c1e23dfe1a8b608839c441340ee7119150320962
Saharat Thongin <was.dew4546@gmail.com>
2024-08-24 11:30:20 +0700

    feat(page): Implement login page
    - Implement page to use as login website
    - Implement TextField component
    - Create type for auth and user api

---
+++ src/types/user.type.ts
@@ -0,0 +1,14 @@
+export type TGetUserProfileResponse = {
+  id: string
+  email: string
+  username: string
+  createdAt: string
+  updatedAt: string
+}
+export type TUserProfile = {
+  id: string
+  email: string
+  username: string
+  createdAt: Date
+  updatedAt: Date
+}

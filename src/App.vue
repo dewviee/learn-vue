2e4d21489df132302f192346b97a9262299f59f8
Saharat Thongin <was.dew4546@gmail.com>
2024-08-24 11:50:42 +0700

    enhance(css): Enhance App CSS
    - To have default black theme.
    - For not destroy my eyes 🔥👀
    - Enhance button styles
    - Enhance input text area styles

--- src/App.vue
+++ src/App.vue
@@ -17,7 +17,42 @@ export default defineComponent({
 #app {
   font-family: Avenir, Helvetica, Arial, sans-serif;
   text-align: center;
-  color: #2c3e50;
+  color: #ffffff;
   margin-top: 60px;
+}
+
+body {
+  background-color: #323232;
+}
+
+a {
+  color: cornflowerblue;
+  text-decoration: none;
+}
+
+a:hover {
+  text-decoration: underline;
+}
+
+button {
+  background-color: #444444;
+  color: #ffffff;
+  border: none;
+  padding: 10px 20px;
+  border-radius: 5px;
+  cursor: pointer;
+}
+
+button:hover {
+  background-color: #555555;
+}
+
+input,
+textarea {
+  background-color: #444444;
+  color: #ffffff;
+  border: 1px solid #666666;
+  padding: 10px;
+  border-radius: 5px;
 }
 </style>

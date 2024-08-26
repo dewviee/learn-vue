3467252739a812b858df266590f10a310f0e61ee
Saharat Thongin <was.dew4546@gmail.com>
2024-08-26 16:51:57 +0700

    Added dayjs package
    - Implement in main.ts
    - Add buddistEra extension to dayjs

--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,8 @@
 import { createApp } from 'vue'
 import App from './App.vue'
 import router from './router/router'
+import dayjs from 'dayjs'
+import buddhistEra from 'dayjs/plugin/buddhistEra'
 
 createApp(App).use(router).mount('#app')
+dayjs.extend(buddhistEra)
